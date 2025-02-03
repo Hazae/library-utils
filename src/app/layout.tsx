f399db809fea8cdf6203b267fc5e994cab3cba72
yena kwon <ewko0220@gmail.com>
2025-02-03 09:42:52 +0900

    Configuration

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Noto_Sans_KR } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const notoSans = Noto_Sans_KR({
+  variable: "--font-noto-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "사람수세기",
+  description: "도서관에 온 사람 수를 세보자",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${notoSans} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
